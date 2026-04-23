fdc41ce03580f99712fe2f59a31ea10b354dac0f
Gabriel Orrico <gabrielorricoxr@gmail.com>
2026-04-22 21:57:22 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mdc(int a, int b) {
+    a = abs(a);
+    b = abs(b);
+
+    while (b != 0) {
+        int resto = a % b;
+        a = b;
+        b = resto;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2, resultado_mmc;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+
+    if (num1 == 0 || num2 == 0) {
+        resultado_mmc = 0;
+    } else {
+        resultado_mmc = abs((num1 * num2) / mdc(num1, num2));
+    }
+
+    printf("O MMC de %d e %d e: %d\n", num1, num2, resultado_mmc);
+
+    return 0;
+}
