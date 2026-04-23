ae9be4218fec933b19c15e316ea28716e32aa440
Gabriel Orrico <gabrielorricoxr@gmail.com>
2026-04-22 22:21:54 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n1, n2, n3, temp;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+
+    printf("Digite o terceiro numero: ");
+    scanf("%d", &n3);
+
+    if (n1 < n2) {
+        temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
+
+    if (n1 < n3) {
+        temp = n1;
+        n1 = n3;
+        n3 = temp;
+    }
+
+    if (n2 < n3) {
+        temp = n2;
+        n2 = n3;
+        n3 = temp;
+    }
+
+    printf("Ordem decrescente: %d, %d, %d\n", n1, n2, n3);
+
+    system("pause");
+    return 0;
+}
